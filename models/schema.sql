400ab35ee614a1de300d5ecdcea19b898d439c86
dezroge <desmondrogers2@gmail.com>
2019-04-16 03:52:00 -0500

    commented out create table

--- models/schema.sql
+++ models/schema.sql
@@ -2,27 +2,27 @@
 CREATE DATABASE stray_pets_db;
 USE stray_pets_db;
 
-CREATE TABLE strays (
-	`Animal ID` VARCHAR
-(255),
-	`Found Location` VARCHAR
-(255),
-	`At AAC` BOOLEAN DEFAULT FALSE,
-	`Intake Date` VARCHAR
-(255),
-	`Type` VARCHAR
-(255),
-	`Looks Like` VARCHAR
-(255),
-	`Color` VARCHAR
-(255),
-	`Sex` VARCHAR
-(255),
-	`Age` VARCHAR
-(255),
-	`Image Link` VARCHAR
-(255)
-);
+-- CREATE TABLE strays (
+-- 	`Animal ID` VARCHAR
+-- (255),
+-- 	`Found Location` VARCHAR
+-- (255),
+-- 	`At AAC` BOOLEAN DEFAULT FALSE,
+-- 	`Intake Date` VARCHAR
+-- (255),
+-- 	`Type` VARCHAR
+-- (255),
+-- 	`Looks Like` VARCHAR
+-- (255),
+-- 	`Color` VARCHAR
+-- (255),
+-- 	`Sex` VARCHAR
+-- (255),
+-- 	`Age` VARCHAR
+-- (255),
+-- 	`Image Link` VARCHAR
+-- (255)
+-- );
 
-SELECT *
-FROM strays;
+-- SELECT *
+-- FROM strays;
