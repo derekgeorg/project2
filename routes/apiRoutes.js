d4e3fab5e980755c586943ae16909ebb1baa2446
dezroge <desmondrogers2@gmail.com>
2019-04-16 03:46:53 -0500

    added routes for pet data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,12 +8,26 @@ module.exports = function(app) {
         });
     });
 
+    // Get all strays
+    app.get("/api/strays", function(req, res) {
+        db.Stray.findAll({}).then(function(dbStrays) {
+            res.json(dbStrays);
+        });
+    });
     // Create a new example
     app.post("/api/examples", function(req, res) {
         db.Example.create(req.body).then(function(dbExample) {
             res.json(dbExample);
         });
     });
+    
+    // Create a new stray
+    app.post("/api/strays", function(req, res) {
+        db.Stray.create(req.body).then(function(dbStray) {
+            res.json(dbStray);
+        });
+    });
+ 
 
     // Delete an example by id
     app.delete("/api/examples/:id", function(req, res) {
