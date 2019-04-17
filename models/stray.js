69a5ccaf5ae4bac7fc530f64de8440465cd6b549
dezroge <desmondrogers2@gmail.com>
2019-04-17 17:04:51 -0500

    updates

--- models/stray.js
+++ models/stray.js
@@ -7,36 +7,20 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             type: DataTypes.INTEGER
         },
-        image: {
-            type: Sequelize.JSON,
-            /*
-            type: Sequelize.TEXT,
-            get: function () {
-                return JSON.parse(this.getDataValue("image"));
-            },
-            set: function (image) {
-                this.setDataValue("image", JSON.stringify(image))
-            }
-            */
-            get(image) {
-                return typeof this.getDataValue(image) === "string" ? JSON.parse(this.getDataValue(image)) : this.getDataValue(image);
-            }
-        },
-        at_aac: DataTypes.BOOLEAN,
+        image: Sequelize.JSON,
+        at_aac:  DataTypes.BOOLEAN,  
         color: DataTypes.STRING,
         sex: DataTypes.STRING,
         animal_id: DataTypes.STRING,
-        location: {
-            type: DataTypes.JSON
-        },
+        location: DataTypes.JSON,
         type: DataTypes.STRING,
         looks_like: DataTypes.STRING,
-        age: DataTypes.STRING,
+        age: DataTypes. STRING,  
         intake_date: DataTypes.DATE
     }, {
-            // don't add the timestamp attributes (updatedAt, createdAt)
-            timestamps: false,
-        });
+        // don't add the timestamp attributes (updatedAt, createdAt)
+        timestamps: false,
+    });
     Stray.associate = function (models) {
         // associations can be defined here
     };
