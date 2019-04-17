b0bd22b783e35e31e221fd7aaed883a3819687d0
dezroge <desmondrogers2@gmail.com>
2019-04-17 02:12:32 -0500

    copied from user.js migration

---
+++ migrations/20190417070422-user.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+    
+          Example:
+          return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        */
+        return queryInterface.createTable("Users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true
+                }
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+    
+          Example:
+          return queryInterface.dropTable('users');
+        */
+        return queryInterface.dropTable("Users")
+    }
+};
