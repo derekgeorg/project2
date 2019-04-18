7694199fbd63af803a500b119264bb73511fddb8
dezroge <desmondrogers2@gmail.com>
2019-04-18 18:37:50 -0500

    timestamp false to user model

--- models/user.js
+++ models/user.js
@@ -27,6 +27,8 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         }
+    }, {
+        timestamp: false
     });
 
     User.associate = function (models) {
