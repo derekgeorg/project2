9d5c0f8d0108ce3e6f2ab727137253a6f089cd1e
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-04-17 20:32:32 -0500

    restyled landing page

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
 $(function () {
+
+
     //search database
     $("#searchSubmit").on("click", function (event) {
+
         var newSearch = {
             breed: $("#breedSearch").val().trim(),
             color: $("#colorSearch").val().trim(),
@@ -8,7 +11,7 @@ $(function () {
             age: $("#ageSearch").val().trim()
         };
 
-        $.ajax("/api/strays", {
+        $.ajax("/api/search", {
             type: "GET",
             data: newSearch
         }).then(function (data) {
@@ -31,7 +34,62 @@ $(function () {
     });
 
     //submit lost pet
-    
+    $("#lostSubmit").on("click", function (event) {
 
+        console.log($("#breedLost").val().trim());
+        console.log($("#colorLost").val().trim());
+        console.log($("#sexLost").val().trim());
+        console.log($("#ageLost").val().trim());
+
+        $("#SubmitModal").show();
+        $(".close").on("click", function () {
+            $("#SubmitModal").hide();
+        });
+
+        event.preventDefault();
+
+        // var lostSearch = {
+        //     breed: $("#breedLost").val().trim(),
+        //     color: $("#colorLost").val().trim(),
+        //     sex: $("#sexLost").val().trim(),
+        //     age: $("#ageLost").val().trim()
+        // };
+
+        // $.ajax("/api/lost-pet", {
+        //     type: "POST",
+        //     data: lostSearch
+        // }).then(function (response) {
+        //     console.log("Submitting new lost pet");
+
+        //     $("#SubmitModal").show();
+        //     $(".close").on("click", function () {
+        //         $("#SubmitModal").hide();
+        //     });
+
+        // });
+    });
     //submit found pet
+    $("#foundSubmit").on("click", function (event) {
+
+        $("#SubmitModal").show();
+        $(".close").on("click", function () {
+            $("#SubmitModal").hide();
+        });
+
+        event.preventDefault();
+
+        var foundSearch = {
+            breed: $("#breedFound").val().trim(),
+            color: $("#colorFound").val().trim(),
+            sex: $("#sexFound").val().trim(),
+            age: $("#ageFound").val().trim()
+        };
+
+        // $.ajax("/api/found-pet", {
+        //     type: "POST",
+        //     data: foundSearch
+        // }).then(function (response) {
+        //     console.log("Submitting new found pet");
+        // });
+    });
 });
