e67c517884d0448d0c37589fa47dbe87a91e9ed8
dezroge <desmondrogers2@gmail.com>
2019-04-18 03:27:02 -0500

    get route for search

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+var Op = Sequelize.Op;
 
 let data = [
     {
@@ -16,61 +18,90 @@ let data = [
     }
 ];
 
-module.exports = function(app) {
-//WILL ASSUME THAT THE MODEL IS Stray
-    app.get("/api/search", function(req, res){
-        // db.Stray.findAll({
-        //     where: {
-
-        //         //allowing only one option will be limiting maybe implement a filter by category
-        //         "Looks Like": "Snowshoe Mix",
-        //         Color: req.body.color,
-        //         // Sex: req.body.sex,
-        //         // Age: req.body.age
-        //     }
-        // }).then(function(dbStrays) {
-        res.render("search", {example: data});
-        // });
+module.exports = function (app) {
+    //WILL ASSUME THAT THE MODEL IS Stray
+    app.get("/api/search", function (req, res) {
+        db.Stray.findAll({
+            where: {
+                // allowing only one option will be limiting maybe implement a filter by category
+                [Op.and]: [
+                    {type: req.body.type},
+                    {
+                        color: {
+                            [Op.like]: `%${req.body.color}%`
+                        }
+                    }
+                ],
+                [Op.or]: [
+                    {
+                        looks_like: {
+                            [Op.like]: `%${req.body.looks_like}%`
+                        }
+                    },
+                    {
+                        sex: req.body.sex
+                    },
+                    {
+                        age: req.body.age
+                    }
+                ]
+                // SELECT * FROM post WHERE type = Cat AND color = Black 
+                // AND looks_like OR sex OR age;
+            }
+        }).then(function (dbStrays) {
+            // only res method else Error: 'Can't set headers after sent to client' 
+            // res.render("search", {dbStrays});  
+            res.json(dbStrays);
+        });
     });
 
-    app.post("/api/lost-pet", function(req, res){
-        db.Stray.create(req.body).then(function(dbStrays) {
+    app.post("/api/lost-pet", function (req, res) {
+        db.Stray.create(req.body).then(function (dbStrays) {
+            res.json(dbStrays);
+        });
+    });
+
+    // for saved searches
+    app.post("/api/saved", function (req, res) {
+        db.Stray.create(req.body).then(function (dbStrays) {
             res.json(dbStrays);
         });
     });
 
     //not sure how we are structuring the data and how we are working with found/reunited pets
-    app.put("/api/found-pet", function(req, res) {
-        db.Stray.update(req.body, {where: {
-            id: req.body.id
-        }
-        }).then(function(dbStrays){
+    app.put("/api/found-pet", function (req, res) {
+        db.Stray.update(req.body, {
+            where: {
+                id: req.body.id
+            }
+        }).then(function (dbStrays) {
             res.json(dbStrays);
         });
     });
 
     // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
+    app.get("/api/examples", function (req, res) {
+        db.Example.findAll({}).then(function (dbExamples) {
             res.json(dbExamples);
         });
     });
 
     // Get all strays
-    app.get("/api/strays", function(req, res) {
-        db.Stray.findAll({}).then(function(dbStrays) {
+    app.get("/api/strays", function (req, res) {
+        db.Stray.findAll({}).then(function (dbStrays) {
             res.json(dbStrays);
         });
     });
+
     // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
+    app.post("/api/examples", function (req, res) {
+        db.Example.create(req.body).then(function (dbExample) {
             res.json(dbExample);
         });
     });
-    
+
     // Create a new lost pet
-    app.post("/api/lost-pet", function(req, res) {
+    app.post("/api/lost-pet", function (req, res) {
         db.Stray.create({
             // Post Image
             // image: req.body.image,
@@ -81,14 +112,14 @@ module.exports = function(app) {
             type: req.body.type,
             looks_like: req.body.looks_like,
             age: req.body.age,
-            intake_date: req.body.intake_date,
-        }).then(function(dbStrays) {
+            intake_date: new Date()
+        }).then(function (dbStrays) {
             res.json(dbStrays);
         });
     });
 
     // Create a new found pet
-    app.post("/api/found-pet", function(req, res) {
+    app.post("/api/found-pet", function (req, res) {
         db.Stray.create({
             // Post Image
             // image: req.body.image,
@@ -99,19 +130,15 @@ module.exports = function(app) {
             type: req.body.type,
             looks_like: req.body.looks_like,
             age: req.body.age,
-            intake_date: req.body.intake_date,
-        }).then(function(dbStrays) {
+            intake_date: new Date()
+        }).then(function (dbStrays) {
             res.json(dbStrays);
         });
     });
 
-
-
- 
-
     // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    app.delete("/api/examples/:id", function (req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
             res.json(dbExample);
         });
     });
