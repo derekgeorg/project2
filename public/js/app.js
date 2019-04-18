bd7dc7dccf000ee1017a6ff091211328b14c4b20
dezroge <desmondrogers2@gmail.com>
2019-04-18 01:17:11 -0500

    corrected breed identifier

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ $(function () {
     $("#searchSubmit").on("click", function (event) {
 
         var newSearch = {
-            type: $("#breedSearch").val().trim(),
+            looks_like: $("#breedSearch").val().trim(),
             color: $("#colorSearch").val().trim(),
             sex: $("#sexSearch").val().trim(),
             age: $("#ageSearch").val().trim()
@@ -49,7 +49,7 @@ $(function () {
         event.preventDefault();
 
         var lostSearch = {
-            type: $("#breedLost").val().trim(),
+            looks_like: $("#breedLost").val().trim(),
             color: $("#colorLost").val().trim(),
             sex: $("#sexLost").val().trim(),
             age: $("#ageLost").val().trim()
@@ -80,7 +80,7 @@ $(function () {
         event.preventDefault();
 
         var foundSearch = {
-            type: $("#breedFound").val().trim(),
+            looks_like: $("#breedFound").val().trim(),
             color: $("#colorFound").val().trim(),
             sex: $("#sexFound").val().trim(),
             age: $("#ageFound").val().trim()
