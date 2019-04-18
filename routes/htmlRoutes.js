194278b4a5c932b5a9230eb282e7bcd7c6a3500b
Ulises Rangel <uli.atx@gmail.com>
2019-04-18 07:37:41 -0500

    Trying to fit everything together

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,37 +1,20 @@
 const db = require("../models");
 const passport = require("../config/passport");
-const authCheck = require("../config/middleware/authentication");
 const path = require("path");
 
 module.exports = function (app) {
-    // Load index page
-    app.get("/", function (req, res) {
-        db.Example.findAll({}).then(function (dbExamples) {
-            res.render("index", {
-                msg: "Welcome!",
-                examples: dbExamples
-            });
-        });
-    });
-
-    app.get("/login", function (req, res) {
-        res.render("login", req);
-    });
-
-    app.get("/signup", function (req, res) {
-        res.render("signup", req);
-    });
-
-    //this authCheck function either redirects to home if not auth (determined by whether a session id exists for the user)
-    //or lets the authorized user continue(next()) to the handlebar generated user page
-    app.get("/user", authCheck, function (req, res) {
-        console.log(req.user);
-
-        res.render("user", req);
+    
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.post("/login", passport.authenticate("local"), function (req, res) {
-        res.json("/user");
+        res.redirect("/api/strays");
+    });
+
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
     });
 
     app.post("/signup", function (req, res) {
@@ -40,16 +23,7 @@ module.exports = function (app) {
             email: req.body.email,
             password: req.body.password
         }).then(function () {
-            res.redirect("/login");
-        });
-    });
-
-    // Load example page and pass in an example by id
-    app.get("/example/:id", function (req, res) {
-        db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-            res.render("example", {
-                example: dbExample
-            });
+            res.redirect("/api/strays");
         });
     });
 
@@ -65,9 +39,26 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/found.html"));
     });
 
+
+    // Unnecessary unless we want them to be their own page for login and signup
+    // app.get("/login", function (req, res) {
+    //     res.render("login", req);
+    // });
+
+    // app.get("/signup", function (req, res) {
+    //     res.render("signup", req);
+    // });
+
+    //this authCheck function either redirects to home if not auth (determined by whether a session id exists for the user)
+    //or lets the authorized user continue(next()) to the handlebar generated user page
+    // app.get("/user", authCheck, function (req, res) {
+    //     console.log(req.user);
+
+    //     res.redirect("/api/strays", req);
+    // });
+
     // Render 404 page for any unmatched routes
     app.get("*", function (req, res) {
         res.render("404");
     });
 };
-
