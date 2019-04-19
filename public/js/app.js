6fb43cf2be3cdf2e6b9bb92e6090804e4d2bc5a7
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-04-19 18:20:04 -0500

    trying to merge

--- public/js/app.js
+++ public/js/app.js
@@ -26,8 +26,12 @@ function displaySearchResults(res) {
 
 $(document).ready((function () {
 
+    // if (!$("#breedLost").val() ) {
+    //     document.getElementById("lostSubmit").disabled = true;
+    // }
 
-    $(".logout").on("click", function(e){
+
+    $(".logout").on("click", function (e) {
 
         e.preventDefault();
 
@@ -97,27 +101,44 @@ $(document).ready((function () {
 
     //submit lost pet
     $("#lostSubmit").on("click", function (event) {
+        event.preventDefault();
 
+        $("#error-message").text("");
+
+        console.log($("#lostType").val().trim());
         console.log($("#breedLost").val().trim());
         console.log($("#colorLost").val().trim());
         console.log($("#sexLost").val().trim());
         console.log($("#ageLost").val().trim());
+        console.log($("#zipLost").val().trim());
         console.log($("#imgLost").val().trim());
+        
+        if (!$("#lostType").val().trim() || !$("#breedLost").val().trim() || !$("#colorLost").val().trim() || !$("#zipLost").val().trim() || !$("#imgLost").val().trim()) {
+            $("#error-message").text("Please fill in all fields");
+            return;
+        }
 
-        event.preventDefault();
+
 
         var lostSearch = {
+            pet_type: $("#lostType").val().trim(),
             looks_like: $("#breedLost").val().trim(),
             color: $("#colorLost").val().trim(),
             sex: $("#sexLost").val().trim(),
             age: $("#ageLost").val().trim(),
-            image: $("#imgLost").val().trim()
+            location: {
+                zip: $("#zipLost").val().trim()
+            },
+            image: {
+                url: $("#imgLost").val().trim()
+            }
         };
 
         $.ajax("/api/lost-pet", {
             type: "POST",
             data: lostSearch
         }).then(function (response) {
+            console.log(lostSearch);
             console.log("Submitting new lost pet");
 
             $("#breedLost").val("");
@@ -125,6 +146,7 @@ $(document).ready((function () {
             $("#sexLost").val("");
             $("#ageLost").val("");
             $("#imgLost").val("");
+            $("#zipLost").val("");
 
             $("#submitModal").modal();
         });
@@ -132,33 +154,51 @@ $(document).ready((function () {
 
     //submit found pet
     $("#foundSubmit").on("click", function (event) {
+
+        event.preventDefault();
+
+        $("#error-message").text("");
+
+        console.log($("#foundType").val().trim());
         console.log($("#breedFound").val().trim());
         console.log($("#colorFound").val().trim());
         console.log($("#sexFound").val().trim());
         console.log($("#ageFound").val().trim());
+        console.log($("#zipFound").val().trim());
         console.log($("#imgFound").val().trim());
 
-
-        event.preventDefault();
+        if (!$("#foundType").val().trim() || !$("#breedFound").val().trim() || !$("#colorFound").val().trim() || !$("#zipFound").val().trim() || !$("#imgFound").val().trim()) {
+            $("#error-message").text("Please fill in all fields");
+            return;
+        }
+        
 
         var foundSearch = {
+            pet_type: $("#foundType").val().trim(),
             looks_like: $("#breedFound").val().trim(),
             color: $("#colorFound").val().trim(),
             sex: $("#sexFound").val().trim(),
             age: $("#ageFound").val().trim(),
-            image: $("#imgFound").val().trim()
+            location: {
+                zip: $("#zipFound").val().trim()
+            },
+            image: {
+                url: $("#imgFound").val().trim()
+            }
         };
 
         $.ajax("/api/found-pet", {
             type: "POST",
             data: foundSearch
         }).then(function (response) {
+            console.log(foundSearch);
             console.log("Submitting new found pet");
             $("#breedFound").val("");
             $("#colorFound").val("");
             $("#sexFound").val("");
             $("#ageFound").val("");
             $("#imgFound").val("");
+            $("#zipFound").val("");
 
             $("#submitModal").modal();
         });
